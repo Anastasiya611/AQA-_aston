1aaa355aa0cac3cd070118a52a5c0b000515ed23
Anastasiya <asa611@mail.ru>
2024-09-14 21:52:19 +0400

    task1&2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        printThreeWords();
+//        checkSumSign();
     }
+       public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign(){
+        int a = 5;
+        int b = -7;
+
+        if ((a+b)>=0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+
 }
