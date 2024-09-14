50dff0f76d53a4dfe3116e4c0e5ee0d375936e3a
Anastasiya <asa611@mail.ru>
2024-09-15 00:33:53 +0400

    task 1-10

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-//        printThreeWords();
-//        checkSumSign();
-//        printColor();
-//        compareNumbers();
-//        sumNumbers();
-//        positiveNumber();
-//        negativeNumber();
-        doubleString();
+//        printThreeWords(); //Задача1
+//        checkSumSign(); //Задача2
+//        printColor(); //Задача3
+//        compareNumbers(); //Задача4
+//        sumNumbers(); //Задача5
+//        positiveNumber(); //Задача6
+//        negativeNumber(); //Задача7
+//        doubleString(); //Задача8
+//        highGradeYear(); //Задача9
+        replacementArray(); //Задача10
+
     }
 
 //    **********************************************TASK_1**************************************************************
@@ -88,4 +93,34 @@ public class Main {
         for (int i=0; i<a; i++)
         {System.out.println (tekst);}
         }
+    //    **********************************************TASK_9**********************************************************
+    public static void highGradeYear(){
+        int a = 800;
+        boolean year = false;
+        if (a%4==0){
+            if (a%100==0 & a%400!=0)
+            System.out.println(year);
+            else if (a%400==0){
+            year = true;
+            System.out.println(year);
+            }
+            else {
+            year = true;
+            System.out.println(year);
+            }
+        }
+        else
+            System.out.println(year);
+    }
+    //        **********************************************TASK_10**********************************************************
+    public static void replacementArray(){
+        int [] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+          System.out.println(Arrays.toString(arr));
+        for (int i=0; i<arr.length; i++){
+        if (arr[i]==1)
+        arr[i] = 0;
+        else
+        arr[i] = 1;}
+        System.out.println(Arrays.toString(arr));
+    }
     }
