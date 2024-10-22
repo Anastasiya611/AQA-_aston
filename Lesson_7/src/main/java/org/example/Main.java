e79f6372ec0360310fd5a21dc970c06bc72eeb2b
Anastasiya611 <109953994+Anastasiya611@users.noreply.github.com>
2024-10-22 22:22:39 +0400

    Update Main.java

--- Lesson_7/src/main/java/org/example/Main.java
+++ Lesson_7/src/main/java/org/example/Main.java
@@ -1,9 +1,1 @@
-package org.example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
