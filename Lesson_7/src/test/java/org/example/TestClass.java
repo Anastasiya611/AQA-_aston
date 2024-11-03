a3f762ff2a441c00e7fb4debf4600c2c5d9c7b51
Anastasiya <asa611@mail.ru>
2024-11-03 16:34:11 +0400

    finish

--- Lesson_7/src/test/java/org/example/TestClass.java
+++ Lesson_7/src/test/java/org/example/TestClass.java
@@ -1,11 +1,7 @@
 package org.example;
 
 import org.PaymentPage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +11,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.MainPage;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import io.qameta.allure.*;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+
 
 class TestClass {
     private WebDriver driver;
@@ -25,7 +25,9 @@ class TestClass {
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
-
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
     @BeforeEach
     void setupTest() {
         driver = new ChromeDriver();
@@ -36,12 +38,18 @@ class TestClass {
         mainPage.acceptCookiesIfPresent();
     }
 
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @AfterEach
     void teardown() {
         driver.quit();
     }
 
     @Test
+    @Epic("Проверка заголовка")
+    @Description("Тест проверяет корректность отображения заголовка.")
     void testTitle() {
         String expectedTitle = "Онлайн пополнение\n" + "без комиссии";
         String actualTitle = mainPage.getTitleBlockText();
@@ -49,6 +57,7 @@ class TestClass {
     }
 
     @Test
+    @Description("Тест проверяет корректность отображения изображений платежных систем.")
     public void testImage() {
         String[] expectedAlts = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"};
         for (String expectedAlt : expectedAlts) {
@@ -64,17 +73,20 @@ class TestClass {
     }
 
     @Test
+    @Epic("Проверка ссылок на информационные сервисы")
+    @Description("Тест проверяет корректность перехода на страницу информации о порядке оплаты.")
     public void testUrlInfoServis() {
-        mainPage.clickOnInfoServiceLink();
-        String currentUrl = driver.getCurrentUrl();
-        assert (currentUrl.equals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
-            /* {System.out.println("Ссылка работает.");
-        } else {
-            System.out.println("Ссылка не работает.");
-        }*/
-    }
+    mainPage.clickOnInfoServiceLink();
+
+    String currentUrl = driver.getCurrentUrl();
+
+            Allure.addAttachment("Текущий URL", currentUrl);
+
+            Assertions.assertEquals(currentUrl, "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", "URL страницы соответствует ожидаемому");
+        }
 
     @Test
+    @Description("Тест проверяет корректность заполнения полей и отображение полей во фрейме.")
     public void testPay() {
         mainPage.fillPhoneNumber("297777777");
         mainPage.fillSum("100");
@@ -87,43 +99,48 @@ class TestClass {
 
 
         assert (paymentPage.isPaymentTitleVisible()); {
-        //    System.out.println("Кнопка работает.");
         }
 
         String expectedpayDescriptionCost = "100.00 BYN";
         String actualpayDescriptionCost = paymentPage.getPayDescriptionCostText();
+        Allure.addAttachment("Фактическое отображение суммы", actualpayDescriptionCost);
         Assertions.assertEquals(expectedpayDescriptionCost, actualpayDescriptionCost);
-       // System.out.println("Отображение 100.00 BYN верно");
+
 
         String expectedColoredDisabled = "Оплатить 100.00 BYN";
         String actualColoredDisabled = paymentPage.getColoredDisabledText();
+        Allure.addAttachment("Фактическое отображение кнопки с суммой", actualColoredDisabled);
         Assertions.assertEquals(expectedColoredDisabled, actualColoredDisabled);
-       // System.out.println("Отображение Оплатить 100.00 BYN верно");
+
 
         String expectedPayDescriptionText = "Оплата: Услуги связи Номер:375297777777";
         String actualPayDescriptionText = paymentPage.getPayDescriptionTextText();
+        Allure.addAttachment("Фактическое отображение оплаты", actualColoredDisabled);
         Assertions.assertEquals(expectedPayDescriptionText, actualPayDescriptionText);
-        //System.out.println("Отображение Номер:375297777777 верно");
+
 
         String expectedNumberCart = "Номер карты";
         String actualNumberCart = paymentPage.getNumberCartText();
+        Allure.addAttachment("Фактическое отображение надписи номер карты", actualNumberCart);
         Assertions.assertEquals(expectedNumberCart, actualNumberCart);
-       // System.out.println("Отображение Номер карты верно");
+
 
         String expectedValidityPeriod = "Срок действия";
         String actualValidityPeriod = paymentPage.getValidityPeriodText();
+        Allure.addAttachment("Фактическое отображение надписи срок действия", actualValidityPeriod);
         Assertions.assertEquals(expectedValidityPeriod, actualValidityPeriod);
-       // System.out.println("Отображение Срок действия верно");
+
 
         String expectedCvc = "CVC";
         String actualCvc = paymentPage.getCvcText();
+        Allure.addAttachment("Фактическое отображение cvc", actualCvc);
         Assertions.assertEquals(expectedCvc, actualCvc);
-       // System.out.println("Отображение CVC верно");
+
 
         String expectedNamePerson = "Имя держателя (как на карте)";
         String actualNamePerson = paymentPage.getNamePersonText();
+        Allure.addAttachment("Фактическое отображение имени держателя", actualNamePerson);
         Assertions.assertEquals(expectedNamePerson, actualNamePerson);
-        //System.out.println("Отображение Имя держателя верно");
 
         paymentPage.displayedVisa();
         paymentPage.displayedMastercard();
@@ -134,54 +151,62 @@ class TestClass {
 
         String expectedSumField = "Сумма";
         String actualSumField = mainPage.getSum();
+        Allure.addAttachment("Фактическое отображение поля сумма", actualSumField);
         Assertions.assertEquals(expectedSumField, actualSumField);
 
         String expectedEmailField = "E-mail для отправки чека";
         String actualEmailField = mainPage.getEmail();
+        Allure.addAttachment("Фактическое отображение поля Email", actualEmailField);
         Assertions.assertEquals(expectedEmailField, actualEmailField);
 
     }
 
     @Test
+    @Description("Тест проверяет корректность отображения полей при выборе Услуги связи")
+
     public void testCommunicationServices() {
         mainPage.paySectionFormClick();
         mainPage.communicationServicesClick();
         String expectedPhoneField = "Номер телефона";
         String actualPhoneField = mainPage.getPhone();
+        Allure.addAttachment("Фактическое отображение номера телефона", actualPhoneField);
         Assertions.assertEquals(expectedPhoneField, actualPhoneField);
        testPaymentOptions();
     }
 
     @Test
+    @Description("Тест проверяет корректность отображения полей при выборе Домашний интернет")
     public void testHomeInternet() {
         mainPage.paySectionFormClick();
         mainPage.homeInternetClick();
         String expectedPhoneField = "Номер телефона";
         String actualPhoneField = mainPage.getPhone();
+        Allure.addAttachment("Фактическое отображение кнопки с суммой", actualPhoneField);
         Assertions.assertEquals(expectedPhoneField, actualPhoneField);
-        //System.out.println("Отображение Номер телефона верно");
         testPaymentOptions();
     }
 
     @Test
+    @Description("Тест проверяет корректность отображения полей при выборе Рассрочка")
     public void testInstallment() {
         mainPage.paySectionFormClick();
         mainPage.installmentClick();
         String expectedScoreInstalment = "Номер счета на 44";
         String actualScoreInstalment = mainPage.getScoreInstalment();
+        Allure.addAttachment("Фактическое отображение номер счета на 44", actualScoreInstalment);
         Assertions.assertEquals(expectedScoreInstalment, actualScoreInstalment);
-        //System.out.println("Отображение Номер счета на 44 верно");
         testPaymentOptions();
     }
 
     @Test
+    @Description("Тест проверяет корректность отображения полей при выборе Задолженность")
     public void testArrears() {
         mainPage.paySectionFormClick();
         mainPage.arrearsClick();
         String expectedScoreArrears = "Номер счета на 2073";
         String actualScoreArrears = mainPage.getScoreArrears();
+        Allure.addAttachment("Фактическое отображение номер счета на 2073", actualScoreArrears);
         Assertions.assertEquals(expectedScoreArrears, actualScoreArrears);
-        //System.out.println("Отображение Номер счета на 44 верно");
         testPaymentOptions();
     }
 }
